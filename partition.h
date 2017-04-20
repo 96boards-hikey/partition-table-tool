7137222e25e2ebc7d45084d5e85fe2558b84c599
Chen Feng <puck.chen@hisilicon.com>
2017-04-20 15:57:00 +0800

    partition, tool: Change system and cache to code freeze number
    Modify partition table for aosp code freeze
    
    Signed-off-by: Chen Feng <puck.chen@hisilicon.com>

--- partition.h
+++ partition.h
@@ -39,12 +39,11 @@ static const struct partition partition_table_ufs[] =
   {PART_FASTBOOT,         2*1024,      12*1024,       UFS_PART_3},/* fastboot      12M   sdd3 */
   {PART_NVME,             14*1024,   6*1024,          UFS_PART_3},/* nvme          6M    sdd4 */
   /* Items above shall not be changed, unless you can update xloader simualtaneously. */
-
-  {PART_FW_LPM3,          20*1024,  1024,            UFS_PART_3},/* mcuimage      1M    sdd5 */
-  {PART_BOOT,             21*1024,  64*1024,         UFS_PART_3},/* boot          64M   sdd6 */
-  {PART_DTS,              85*1024,  16*1024,         UFS_PART_3},/* dtimage       16M   sdd7 */
-  {PART_TRUSTFIRMWARE,    101*1024,  2*1024,          UFS_PART_3},/* trustfirmware 2M    sdd8 */
-  {PART_CACHE,            103*1024,   256*1024,        UFS_PART_3},/* cache         256M  sdd9 */
+  {PART_CACHE,            20*1024,   256*1024,        UFS_PART_3},/* cache         256M  sdd5 */
+  {PART_FW_LPM3,          276*1024,  1024,            UFS_PART_3},/* mcuimage      1M    sdd6 */
+  {PART_BOOT,             277*1024,  64*1024,         UFS_PART_3},/* boot          64M   sdd7 */
+  {PART_DTS,              341*1024,  16*1024,         UFS_PART_3},/* dtimage       16M   sdd8 */
+  {PART_TRUSTFIRMWARE,    357*1024,  2*1024,          UFS_PART_3},/* trustfirmware 2M    sdd9 */
   {PART_SYSTEM,           359*1024,  4688*1024,       UFS_PART_3},/* system        4688M sdd10 */
   {PART_VENDOR,           5047*1024, 784*1024,        UFS_PART_3},/* vendor        784M  sdd11 */
   {PART_FIP,              5831*1024, 12*1024,         UFS_PART_3},/* fip        12M  sdd12 */
